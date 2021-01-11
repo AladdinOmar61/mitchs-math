c3624cc814dc4ca8d70f3487fc417bca89aa54e4
AladdinOmar61 <aladdin.omar@twgtech.com>
2021-01-11 18:18:54 -0500

    selecting month function on click no works, changed calendar shapes to square instead of circle

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -21,12 +21,27 @@ export default class Calendar extends React.Component {
     return this.state.dateObject.format("MMMM");
   };
 
+  setMonth = (month) => {
+    let monthNo = this.state.allMonths.indexOf(month);
+    let dateObject = Object.assign({}, this.state.dateObject);
+    dateObject = moment(dateObject).set("month", monthNo);
+    this.setState({
+      dateObject,
+    });
+  };
+
   monthList = (props) => {
     let months = [];
     props.data.map((data) => {
       months.push(
-        <td>
-          <span>{data}</span>
+        <td
+          key={data}
+          className="calendar-month-select"
+          onClick={(e) => {
+            this.setMonth(data);
+          }}
+        >
+          <span className="calendar-month">{data}</span>
         </td>
       );
     });
@@ -35,7 +50,7 @@ export default class Calendar extends React.Component {
     let cells = [];
 
     months.forEach((row, i) => {
-      if (i % 3 !== 0 || i == 0) {
+      if (i % 3 !== 0 || i === 0) {
         cells.push(row);
       } else {
         rows.push(cells);
@@ -72,7 +87,7 @@ export default class Calendar extends React.Component {
     let weekdayshortname = this.weekdayShort.map((day) => {
       return (
         <th key={day} className="week-day">
-          <span className="day">{day}</span>
+          <span className="week-day-num">{day}</span>
         </th>
       );
     });
@@ -82,10 +97,10 @@ export default class Calendar extends React.Component {
     }
     let daysInMonth = [];
     for (let d = 1; d <= this.daysInMonth(); d++) {
-      let currentDay = d == this.currentDay() ? "today" : "";
+      let currentDay = d === this.currentDay() ? "today" : "";
       daysInMonth.push(
         <td key={d} className={`calendar-day ${currentDay}`}>
-          <span className="day-num">{d}</span>
+          <span className="days-num">{d}</span>
         </td>
       );
     }
@@ -113,7 +128,16 @@ export default class Calendar extends React.Component {
     return (
       <div className="calendar">
         <div className="tail-datetime-calendar">
-          <div className="calendar-navi">{this.month()}</div>
+          <div className="calendar-navi">
+            <span
+              onClick={(e) => {
+                this.showMonth();
+              }}
+              className="calendar-label"
+            >
+              {this.month()}
+            </span>
+          </div>
         </div>
         <div className="calendar-date">
           <this.monthList data={moment.months()} />
