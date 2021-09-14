f2e14e84588af4261c407eef3da1bbcac61d156b
AladdinOmar61 <aladdin.omar@twgtech.com>
2021-09-14 12:05:14 -0400

    created client ID and login from google function now works, currently styling button

--- src/App.js
+++ src/App.js
@@ -5,15 +5,33 @@ import SelectTimes from "./components/SelectTimes.js";
 import Confirmation from "./components/Confirmation.js";
 // import Landing from "./components/Landing.js";
 import { Route, Switch } from "react-router-dom";
-import GoogleLogin from 'react-google-login';
+import GoogleLogin from "react-google-login";
+
+const responseGoogle = (response) => {
+  console.log(response);
+};
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/">
-          {/* <Landing /> */}
-          <GoogleLogin />
+          <GoogleLogin
+            clientId="476555932517-tb7g4js5krhljvaionc4o3vq3b06l47e.apps.googleusercontent.com"
+            render={(renderProps) => (
+              <button
+                className="google-login"
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+              >
+                <img src="/Images/t-google-icon.png" alt="google-icon" />
+                Sign in From Google
+              </button>
+            )}
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={"single_host_origin"}
+          />
         </Route>
         <Route exact path="/programs">
           <Programs />
